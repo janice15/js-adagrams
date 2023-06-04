0dd6e8f393001d959962a309db85801638c63adb
Janice <janicenguyenn@gmail.com>
2023-06-04 14:23:09 -0700

    add code to randomly get 10 letters in drawLetters()

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,55 @@
 export const drawLetters = () => {
   // Implement this method for wave 1
+  const LETTER_POOL = {
+    A: 9,
+    B: 2,
+    C: 2,
+    D: 4,
+    E: 12,
+    F: 2,
+    G: 3,
+    H: 2,
+    I: 9,
+    J: 1,
+    K: 1,
+    L: 4,
+    M: 2,
+    N: 6,
+    O: 8,
+    P: 2,
+    Q: 1,
+    R: 6,
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1,
+  };
+
+  const letterPool = Object.keys(LETTER_POOL);
+  const hand = [];
+  const letterFrequency = {};
+
+  for (let i = 0; i < 10; i++) {
+    let letter = letterPool[Math.floor(Math.random() * letterPool.length)];
+    if (letter in letterFrequency) {
+      letterFrequency[letter] += 1;
+    } else {
+      letterFrequency[letter] = 1;
+    }
+    
+    if (letterFrequency[letter] <= LETTER_POOL[letter]) {
+      hand.push(letter)
+    }
+
+  } return hand;
 };
+
+  
+
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
